4a19cd67edc1db4e2dc738d24fe720310f2e2920
Alex-Sjoberg <asjoberg22@yahoo.com>
2013-11-08 13:20:07 -0500

    Some odds and ends

---
+++ Rogue Space/testing.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 13, 2013
+
+@author: asjoberg
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        self.assert
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
